d4ab642bcbec5477b69934c00b1bb1aaa73d8907
ghumoindia <ghumooindia1@gmail.com>
2025-09-15 21:18:00 +0530

    added video path

--- src/app.js
+++ src/app.js
@@ -50,6 +50,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/uploads", express.static(path.join(__dirname, "uploads/images")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads/videos")));
 
 // adminRoutes
 app.use(`${adminUrl}/auth`, adminAuthRoutes);
